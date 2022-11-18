d5d4633998a1c76262791a9ef53ac54c99226496
John Wang <405537142@gms.tku.edu.tw>
2022-11-18 13:27:14 +0800

    fix host name

--- api.py
+++ api.py
@@ -182,4 +182,4 @@ def category():
 if __name__ == "__main__":
     app.config["JSON_AS_ASCII"] = False
     app.config["JSON_SORT_KEYS"] = False
-    app.run(port=3000)
+    app.run(host="0.0.0.0", port=3000)
