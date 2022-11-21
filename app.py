0e195d144016ce28a6cffb5c55a7736725fa61b6
Johnwang <s13602507586@gmail.com>
2022-11-21 20:21:25 +0800

    Perf MySQL Query Frequency

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from api import app2
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+app.config["JSON_SORT_KEYS"] = False
 
 # Pages
 @app.route("/")
@@ -20,4 +21,4 @@ def thankyou():
 	return render_template("thankyou.html")
 
 app.register_blueprint(app2)
-app.run(port=3000)
+app.run(host="0.0.0.0", port=3000)
